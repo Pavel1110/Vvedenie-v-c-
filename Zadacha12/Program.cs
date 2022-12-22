a18ab13f9db35f02d7e36411ee013b9dee4e339f
Pavel1110 <120491502+Pavel1110@users.noreply.github.com>
2022-12-22 15:59:35 +0300

    .\Zadacha12\

---
+++ Zadacha12/Program.cs
@@ -0,0 +1,63 @@
+﻿
+//1 вид тетодов
+
+//void Method1()
+//{
+  //  System.Console.WriteLine("Автор Фамилия имя");
+//}
+//это первая группа методов
+//Method1();//таким образом этот метод вызывается
+
+
+
+//2 вид метода
+
+//void Method2(string msg) решение вторым методом
+//{
+//    Console.WriteLine("msg");
+//}
+//Method2(msg:"текст сообщения");
+
+
+
+//void Method21(string msg,int count) решение вторым методом
+//{
+  //   int i = 0;
+    //while (i < count)
+     //{
+       // Console.WriteLine(msg);
+        //i++;
+     //}
+//}
+//Method21(msg:"Текст",count: 4);порядок расположения аргументов в этих скобках НЕВАЖЕН!
+
+
+
+//3 вид метода
+
+//int Method3()              Метод №3
+//{
+  //return DateTime.Now.Year;
+//}
+//int year = Method3();
+//Console.WriteLine(year);
+
+
+
+
+//4 вид метода
+
+//string Method4(int count,string text)
+//{
+  //  i=0;
+    //string result = string.Empty; // результат = пустой строке
+    //while (int < count)
+    //{
+      //result = result + text;
+      //i++;
+    //}
+    //return result;
+//}
+
+//string res = Method4 (10 "z");
+//Console.WriteLine (res);
