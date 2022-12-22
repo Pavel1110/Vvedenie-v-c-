f638d9e684c3c823e04006a0e3f87a83c26266b8
Pavel1110 <120491502+Pavel1110@users.noreply.github.com>
2022-12-22 16:26:25 +0300

    .\Cikles\

---
+++ Cikles/Program.cs
@@ -0,0 +1,32 @@
+﻿
+//ЦИКЛ FOR
+
+//string Method4(int count,string text)
+//{
+    
+  //  string result = string.Empty; // результат = пустой строке
+    //for(int i=0;i < count;i++)
+    //{
+      //result = result + text;
+      
+    //}
+    //return result;
+//}
+
+//string res = Method4 (10, "z");
+//Console.WriteLine (res);
+
+
+
+
+
+//Цикл в цикле
+
+for (int i = 2; i <= 10; i++)    //внешний цикл
+{
+   for (int j = 2; j <= 10; j++)      //внутренний цикл
+    {
+        Console.WriteLine ($"{i} * {j} = {i*j}");  // способ интерполяций строк
+    }
+    Console.WriteLine();
+}
